2dcfd9ccdf08ce31852a34bc067bef7e9a62a66d
jorge <jborras15@gmail.com>
2022-08-29 16:51:15 -0500

    Proyecto para Manejo de presupuesto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ManejoProsupuesto.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
