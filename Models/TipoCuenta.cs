465b09e670c8bb4e17bc59e29f97bf37c937fe8b
jorge <jborras15@gmail.com>
2022-08-30 13:06:54 -0500

    Validaciones personalizadas por atributo
    se cambio el tipo de validacion de sumary de all a ModeOnly para que solo muestre en ese div los errores del modelo . en Crear.cshtml  y asi en los span muestar los errore de atributo.
    
    se creo el archivo PrimeraLetraMayusculaAttribute.cs el cual sirve para validaciones si el nombre se ingresa con la primera letra en mayucusla
    
    TipoCuenta.cs se agrego la anotacion de PrimeraLetraMayusculaAttribute para hacer las respectiva validaciones

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using ManejoProsupuesto.Validaciones;
 
 namespace ManejoProsupuesto.Models;
 
@@ -8,6 +9,7 @@ public class TipoCuenta
     // {0} hace referencia al campo actual en la vista
     // con display podemos colocar el nombre del label 
     [Required (ErrorMessage = "EL campo {0} es requerido")]
+    [PrimeraLetraMayuscula]
     public string Nombre { get; set; }
     public int UsuarioId { get; set; }
     public int Orden { get; set; }
