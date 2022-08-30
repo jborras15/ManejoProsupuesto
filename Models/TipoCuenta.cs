824223967785e0c16c4d7239f824fd7a904b65a8
jorge <jborras15@gmail.com>
2022-08-30 10:45:18 -0500

    usamos display para colocarle el nombre el label.

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -6,8 +6,11 @@ public class TipoCuenta
 {
     public int Id { get; set; }
     // {0} hace referencia al campo actual en la vista
+    // con display podemos colocar el nombre del label 
     [Required (ErrorMessage = "EL campo {0} es requerido")]
-    [StringLength(maximumLength:50, MinimumLength = 3,ErrorMessage = "La longitud del {0} debe estar entre {1} y {2}")]
+    [StringLength(maximumLength:50, MinimumLength = 3,ErrorMessage = 
+        "La longitud del {0} debe estar entre {1} y {2}")]
+    [Display(Name = "El nombre de tipo cuenta")]
     public string Nombre { get; set; }
     public int UsuarioId { get; set; }
     public int Orden { get; set; }
